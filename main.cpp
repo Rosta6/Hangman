ba1bc28710dbb15e7067b0d25208330449cf2d27
Rostislav Kubin <rostislav.kubin1@gmail.com>
2019-08-04 10:54:01 +0200

    more new words

--- main.cpp
+++ main.cpp
@@ -9,12 +9,17 @@
 #include <iostream>
 #include <string>
 #include <cstring>                  //na pouziti strlen
+#include <fstream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <stdio.h>      /* printf, NULL */
 using namespace std;
-char hledaneSlovo[] = "lednice";    //hledane slovo
 int zivoty = 5;                     //nastaveni poctu zivotu
 char pismeno;                       //pismeno nactene z klavesnice
 bool game = true;                   //podminka pro while cyklus
 bool podminka;                      //podminka pro odecitani zivotu
+string arr_slova[10];
+int vstup;
 
 void vykresli(char x[], char y[]){                  //vykraslovaci funkce
         cout << "** HANGMAN **" << endl;
@@ -26,8 +31,26 @@ void vykresli(char x[], char y[]){                  //vykraslovaci funkce
         cout << "*************" << endl;
 }
 
+
+
 int main(){
-    char odkryvaneSlovo[strlen(hledaneSlovo)+1];
+    start:srand (time(NULL));
+    zivoty = 5;
+    int n = rand()%10 + 1;
+    ifstream file("slova.txt");
+    if(file.is_open())
+    {
+
+        for(int i = 0; i < 10; i++)
+        {
+            file >> arr_slova[i];
+        }
+    }
+    string vybraneSlovo = arr_slova[n];
+    char hledaneSlovo[vybraneSlovo.length()];
+    strcpy(hledaneSlovo, vybraneSlovo.c_str()); 
+   
+    char odkryvaneSlovo[strlen(hledaneSlovo)];
     for (unsigned i = 0; i < strlen(hledaneSlovo); i++){    //priprava prazdneho slova
         odkryvaneSlovo[i] = '_';
     }
@@ -56,16 +79,45 @@ int main(){
         if(hledaneSlovo[i] == odkryvaneSlovo[i]){
             end_game = end_game + 1;
         }}
-        if(end_game == strlen(odkryvaneSlovo)){
-           game = false;
+        if(end_game == vybraneSlovo.length()){
            cout << "Gratuluji, vyhral si!" << endl;
+           cout << "Chces hrat znovu? \n(1) Ano\n(2) Ne"<< endl;
+           cin >> vstup;
+           switch (vstup)
+           {
+            case 1:
+               system("cls");
+               goto start;  
+            break;
+            case 2:
+                game = false;
+            break;
+           
+           default:
+               game = false;
+               break;
+           }
        }
         if(zivoty == 0){
-            game = false;
             cout << "Smolik, prohral jsi." << endl;
+            cout << "Chces hrat znovu? \n(1) Ano\n(2) Ne"<< endl;
+           cin >> vstup;
+           switch (vstup)
+           {
+            case 1:
+               system("cls");
+               goto start;  
+            break;
+            case 2:
+                game = false;
+            break;
+           
+           default:
+               game = false;
+               break;
+           }
         }
     }   
-    system("pause");
     
     return 0;
 }
